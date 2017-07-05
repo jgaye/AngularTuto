7e8031d255f00c116d7c5016f6090edeba61c734
jgaye <jgaye@point.io>
2017-07-05 17:38:02 +0200

    Finished part5 of tuto
    * You added the Angular router to navigate among different components.
    * You learned how to create router links to represent navigation menu
    items.
    * You used router link parameters to navigate to the details of the
    user-selected hero.
    * You shared the HeroService among multiple components.
    * You moved HTML and CSS out of the component file and into their own
    files.
    * You added the uppercase pipe to format data.

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -15,7 +15,8 @@ var AppComponent = (function () {
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n\t\t<h1>{{title}}</h1>\n\t\t<nav>\n\t\t\t<a routerLink=\"/dashboard\">Dashboard</a>\n\t\t\t<a routerLink=\"/heroes\">Heroes</a>\n\t\t</nav>\n\t\t<router-outlet></router-outlet>\n\t"
+        template: "\n\t  <h1>{{title}}</h1>\n\t  <nav>\n\t    <a routerLink=\"/dashboard\" routerLinkActive=\"active\">Dashboard</a>\n\t    <a routerLink=\"/heroes\" routerLinkActive=\"active\">Heroes</a>\n\t  </nav>\n\t  <router-outlet></router-outlet>\n\t",
+        styleUrls: ['./app.component.css']
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- src/app/heroes.component.js
+++ src/app/heroes.component.js
@@ -9,10 +9,12 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
 var hero_service_1 = require("./hero.service");
 var HeroesComponent = (function () {
-    function HeroesComponent(heroService) {
+    function HeroesComponent(heroService, router) {
         this.heroService = heroService;
+        this.router = router;
     }
     HeroesComponent.prototype.getHeroes = function () {
         var _this = this;
@@ -24,15 +26,19 @@ var HeroesComponent = (function () {
     HeroesComponent.prototype.onSelect = function (hero) {
         this.selectedHero = hero;
     };
+    HeroesComponent.prototype.gotoDetail = function () {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    };
     return HeroesComponent;
 }());
 HeroesComponent = __decorate([
     core_1.Component({
         selector: 'my-heroes',
-        template: "\n\t    <h2>My Heroes</h2>\n\t    <ul class=\"heroes\">\n\t      \t<li *ngFor=\"let hero of heroes\" [class.selected]=\"hero === selectedHero\" (click)=\"onSelect(hero)\">\n\t        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t      </li>\n\t    </ul>\n\t    <hero-detail [hero]=\"selectedHero\"></hero-detail>\n  \t",
-        styles: ["\n\t    .selected {\n\t      background-color: #CFD8DC !important;\n\t      color: white;\n\t    }\n\t    .heroes {\n\t      margin: 0 0 2em 0;\n\t      list-style-type: none;\n\t      padding: 0;\n\t      width: 15em;\n\t    }\n\t    .heroes li {\n\t      cursor: pointer;\n\t      position: relative;\n\t      left: 0;\n\t      background-color: #EEE;\n\t      margin: .5em;\n\t      padding: .3em 0;\n\t      height: 1.6em;\n\t      border-radius: 4px;\n\t    }\n\t    .heroes li.selected:hover {\n\t      background-color: #BBD8DC !important;\n\t      color: white;\n\t    }\n\t    .heroes li:hover {\n\t      color: #607D8B;\n\t      background-color: #DDD;\n\t      left: .1em;\n\t    }\n\t    .heroes .text {\n\t      position: relative;\n\t      top: -3px;\n\t    }\n\t    .heroes .badge {\n\t      display: inline-block;\n\t      font-size: small;\n\t      color: white;\n\t      padding: 0.8em 0.7em 0 0.7em;\n\t      background-color: #607D8B;\n\t      line-height: 1em;\n\t      position: relative;\n\t      left: -1px;\n\t      top: -4px;\n\t      height: 1.8em;\n\t      margin-right: .8em;\n\t      border-radius: 4px 0 0 4px;\n\t    }\n  \t"]
+        templateUrl: './heroes.component.html',
+        styleUrls: ['./heroes.component.css']
     }),
-    __metadata("design:paramtypes", [hero_service_1.HeroService])
+    __metadata("design:paramtypes", [hero_service_1.HeroService,
+        router_1.Router])
 ], HeroesComponent);
 exports.HeroesComponent = HeroesComponent;
 //# sourceMappingURL=heroes.component.js.map
